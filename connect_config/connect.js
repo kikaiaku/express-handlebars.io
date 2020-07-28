7bdcf71e045e052a03a4f9c6db17ed36c90f6489
kikaiaku <tjscarb89@aol.com>
2020-07-28 18:13:43 -0400

    working with ta

--- connect_config/connect.js
+++ connect_config/connect.js
@@ -1,20 +1,19 @@
 var mysql = require("mysql");
 var connection;
-var dotenv = require("dotenv");
-dotenv.config();
-if (process.env.JAWSDB_URL) {
-  console.log("\n\n\n using JAWS \n\n\n");
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  console.log("\n\n\n using local \n\n\n")
-  connection = mysql.createConnection({
-    port: process.env.port,
-    host: "::",
-    user: "root",
-    password: "K1ka1AkuOkam1<363",
-    database: "burgers_db"
+require('dotenv').config();
+// if (process.env.JAWSDB_URL) {
+//   console.log("\n\n\n using JAWS \n\n\n");
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   console.log("\n\n\n using local \n\n\n")
+  let connection = mysql.createConnection({
+    port: 3306,
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
   });
-};
+// };
 
 // Make connection.
 connection.connect(function(e){
