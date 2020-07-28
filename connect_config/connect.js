e61db7c52a32d28913cb27628422f1fe0c5e077f
kikaiaku <tjscarb89@aol.com>
2020-07-27 22:16:06 -0400

    working on heroku

--- connect_config/connect.js
+++ connect_config/connect.js
@@ -1,15 +1,15 @@
 var mysql = require("mysql");
 var connection;
 var dotenv = require("dotenv");
-
 dotenv.config();
+console.log(process.env.JAWSDB_URL);
 if (process.env.JAWSDB_URL) {
   console.log("\n\n\n using JAWS \n\n\n");
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   console.log("\n\n\n using local \n\n\n")
   connection = mysql.createConnection({
-    port: 3306,
+    port: process.env.SERVERPORT,
     host: "127.0.0.1",
     user: "root",
     password: "K1ka1AkuOkam1<363",
